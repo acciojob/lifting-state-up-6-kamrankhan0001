593502ecf6a33a34d687430399c7eeab7164beb3
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-13 01:06:27 +0530

    submitting code-Thu Jul 13 2023 01:06:25 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,46 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+const Parent = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a project', completed: false },
+    { id: 3, text: 'Write documentation', completed: false }
+  ]);
 
-export default App
+  const handleComplete = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: true };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="parent">
+      <Child todos={todos} handleComplete={handleComplete} />
+    </div>
+  );
+};
+
+const Child = ({ todos, handleComplete }) => {
+  return (
+    <div className="child">
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            {!todo.completed && (
+              <button onClick={() => handleComplete(todo.id)}>Complete</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Parent;
